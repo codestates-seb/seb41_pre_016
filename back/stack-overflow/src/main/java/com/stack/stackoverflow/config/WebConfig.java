2f00e2e0d34bc7cad6448daa47ad4c2d79d79345
bimilless <bimilless@gmail.com>
2022-12-21 15:26:48 +0900

    fix: cors error

---
+++ back/stack-overflow/src/main/java/com/stack/stackoverflow/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.stack.stackoverflow.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080");
+    }
+}
