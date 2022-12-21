e2776a1e7623728522c2793aecc48bffa25bf9b0
Double.Pos <wks1945@naver.com>
2022-12-21 14:59:19 +0900

    cors

--- front/stack-overflow/src/App.js
+++ front/stack-overflow/src/App.js
@@ -1,13 +1,31 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello world</p>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const URL = '/test';
+
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+
+      try {
+        const response = await axios.get(URL);
+        console.log(response);
+        setData(response);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  return <div>Hello</div>;
+};
 
 export default App;
