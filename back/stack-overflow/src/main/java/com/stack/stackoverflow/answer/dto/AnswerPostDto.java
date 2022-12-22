c406c042566a888773e4c1d4f33a4b09bf5138e6
bimilless <bimilless@gmail.com>
2022-12-22 17:20:10 +0900

    refactor: dto refactoring

--- back/stack-overflow/src/main/java/com/stack/stackoverflow/answer/dto/AnswerPostDto.java
+++ back/stack-overflow/src/main/java/com/stack/stackoverflow/answer/dto/AnswerPostDto.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.NotBlank;
 @Getter
 public class AnswerPostDto {
 
-    private String questionId;
+    private Long questionId;
     @NotBlank
     private String content;
 }
