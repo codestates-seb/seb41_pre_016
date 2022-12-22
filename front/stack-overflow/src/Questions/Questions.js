ad122d7f664c406eeacc83449b3e7f9e595a66a6
Atia <67799687+AtiaKR@users.noreply.github.com>
2022-12-23 05:57:12 +0900

    feat: add new components

---
+++ front/stack-overflow/src/Questions/Questions.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+import BlueButton from "./Buttons/BlueButton";
+import List from "./List";
+
+const Container = styled.div`
+  max-width: 1100px;
+  width: calc(100% - 164);
+  height: 100vh;
+  background-color: white;
+  padding: 24px;
+  border-left: 1px solid hsl(210, 8%, 85%);
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI Adjusted",
+    "Segoe UI", "Liberation Sans", sans-serif;
+`;
+
+const MainSection = styled.section`
+  float: left;
+  width: 728px;
+  margin: 0;
+  padding: 0;
+`;
+
+const Headbar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  & > h1 {
+    font-size: 27px;
+    font-weight: 400;
+    flex: 1 auto !important;
+    display: inline-block;
+    vertical-align: middle;
+  }
+`;
+
+const Questions = () => {
+  return (
+    <Container>
+      <MainSection>
+        <Headbar>
+          <h1> All Questions </h1>
+          <BlueButton text="Ask Question" />
+        </Headbar>
+        <List />
+      </MainSection>
+    </Container>
+  );
+};
+
+export default Questions;
