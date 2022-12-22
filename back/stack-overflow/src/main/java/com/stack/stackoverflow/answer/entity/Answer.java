bc97bcefef6f7322e1f7831cd47ef4ebaa26afc8
bimilless <bimilless@gmail.com>
2022-12-22 13:31:02 +0900

    fix: Answer entity Modify

--- back/stack-overflow/src/main/java/com/stack/stackoverflow/answer/entity/Answer.java
+++ back/stack-overflow/src/main/java/com/stack/stackoverflow/answer/entity/Answer.java
@@ -1,27 +1,36 @@
 package com.stack.stackoverflow.answer.entity;
 
 import com.stack.stackoverflow.audit.Auditable;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Answer extends Auditable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long answerId;
 
     @Column
-    private String answerContent;
+    private String content;
 
     @Column
-    private Integer answerVote;
+    private Integer vote;
+
+    @Column(name = "LAST_MODIFIED_AT")
+    private LocalDateTime modifiedAt = LocalDateTime.now();
 
     @ManyToOne
     @JoinColumn(name = "QUESTION_ID")
@@ -31,7 +40,6 @@ public class Answer extends Auditable {
     @JoinColumn(name = "USERPAGE_ID")
     private UserPage userPage;
 
-    @OneToMany(mappedBy = "answer")
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "answer")
     private List<Comment> comments = new ArrayList<>();
-
 }
