00544a5e5939d2fc1ad23a62c6598168beda3c73
Atia <67799687+AtiaKR@users.noreply.github.com>
2022-12-23 06:01:30 +0900

    feat: add file for state management using zustand

---
+++ front/stack-overflow/src/Questions/Store/SummaryStates.js
@@ -0,0 +1,86 @@
+import create from "zustand";
+
+const useStore = create((set, get) => ({
+  questions: [
+    {
+      questionId: 1,
+      title: "Suggestion for Automatically Match Data in MysQL",
+      content:
+        "I have limited access to the MySQL database, I just can see a view called customer contains customer_id, name, and their location id_customer name location 1 Andy Detro.it 2",
+      votes: 10,
+      views: 3,
+      answerCount: 4,
+      tags: ["java", "ios"],
+      createdAt: "2022-12-19 10:33:41",
+      modifiedAt: "2022-12-21 10:33:41",
+    },
+    {
+      questionId: 1,
+      title: "Suggestion for Automatically Match Data in MysQL",
+      content:
+        "I have limited access to the MySQL database, I just can see a view called customer contains customer_id, name, and their location id_customer name location 1 Andy Detro.it 2",
+      votes: 10,
+      views: 3,
+      answerCount: 4,
+      tags: ["java", "ios"],
+      createdAt: "2022-12-19 10:33:41",
+      modifiedAt: "2022-12-21 10:33:41",
+    },
+    {
+      questionId: 1,
+      title: "Suggestion for Automatically Match Data in MysQL",
+      content:
+        "I have limited access to the MySQL database, I just can see a view called customer contains customer_id, name, and their location id_customer name location 1 Andy Detro.it 2",
+      votes: 10,
+      views: 3,
+      answerCount: 4,
+      tags: ["java", "ios"],
+      createdAt: "2022-12-19 10:33:41",
+      modifiedAt: "2022-12-21 10:33:41",
+    },
+    {
+      questionId: 1,
+      title: "Suggestion for Automatically Match Data in MysQL",
+      content:
+        "I have limited access to the MySQL database, I just can see a view called customer contains customer_id, name, and their location id_customer name location 1 Andy Detro.it 2",
+      votes: 10,
+      views: 3,
+      answerCount: 4,
+      tags: ["java", "ios"],
+      createdAt: "2022-12-19 10:33:41",
+      modifiedAt: "2022-12-21 10:33:41",
+    },
+    {
+      questionId: 1,
+      title: "Suggestion for Automatically Match Data in MysQL",
+      content:
+        "I have limited access to the MySQL database, I just can see a view called customer contains customer_id, name, and their location id_customer name location 1 Andy Detro.it 2",
+      votes: 10,
+      views: 3,
+      answerCount: 4,
+      tags: [
+        "iosasdadasdsadsaiosasdadasdsadsaiosasdadasdsadsa",
+        "iosasdadasdsadsa",
+        "ASDFASFASCDSADCASDCSADCSAD",
+        "asDCASCASCDASDCSADCVFASDF",
+        "cADSCSADCDSACDSACDSACDSA",
+      ],
+      createdAt: "2022-12-19 10:33:41",
+      modifiedAt: "2022-12-21 10:33:41",
+    },
+    {
+      questionId: 1,
+      title: "Suggestion for Automatically Match Data in MysQL",
+      content:
+        "I have limited access to the MySQL database, I just can see a view called customer contains customer_id, name, and their location id_customer name location 1 Andy Detro.it 2",
+      votes: 10,
+      views: 3,
+      answerCount: 4,
+      tags: ["java", "ios"],
+      createdAt: "2022-12-19 10:33:41",
+      modifiedAt: "2022-12-21 10:33:41",
+    },
+  ],
+}));
+
+export default useStore;
