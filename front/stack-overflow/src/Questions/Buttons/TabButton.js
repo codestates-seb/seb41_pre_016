7f27c9cd1ba6b8d6f52ef01216882546d5dc4877
Atia <67799687+AtiaKR@users.noreply.github.com>
2022-12-23 16:15:28 +0900

    feat: Add tab comp for sorting

---
+++ front/stack-overflow/src/Questions/Buttons/TabButton.js
@@ -0,0 +1,50 @@
+import styled, { css } from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  cursor: pointer;
+
+  div {
+    &:nth-child(1) {
+      border-top-left-radius: 3px;
+      border-bottom-left-radius: 3px;
+    }
+    &:nth-child(2) {
+      border-top-right-radius: 3px;
+      border-bottom-right-radius: 3px;
+    }
+  }
+`;
+
+const Tabbox = styled.div`
+  font-size: 13px;
+  margin-left: -1px;
+  padding: 9.6px;
+  color: var(--black-500);
+  border: 1px solid var(--black-300);
+
+  &:hover {
+    background-color: var(--black-025);
+  }
+
+  ${(props) => {
+    props.selected &&
+      css`
+        color: var(--black-700);
+        border: 1px solid var(--black-400);
+        background-color: var(--black-075);
+      `;
+  }}
+`;
+
+const TabButton = () => {
+  return (
+    <Container>
+      <Tabbox>Newest</Tabbox>
+      <Tabbox>Unanswered</Tabbox>
+    </Container>
+  );
+};
+
+export default TabButton;
