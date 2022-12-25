aeea5855352ca7fde3a1cf002b4f65d75d93c46a
kung036 <sinariari3@gmail.com>
2022-12-26 08:20:10 +0900

    fix: ExceptionCode

--- back/stack-overflow/src/main/java/com/stack/stackoverflow/exception/ExceptionCode.java
+++ back/stack-overflow/src/main/java/com/stack/stackoverflow/exception/ExceptionCode.java
@@ -14,7 +14,9 @@ public enum ExceptionCode {
     USER_NOT_FOUND(404, "User not found"),
     QUESTION_NOT_FOUND(404, "Question not found"),
     USER_EXISTS(409, "User exists"),
-    TAG_NOT_FOUND(404, "Tag not found");
+    LOGIN_FAIL(404, "Password is not match"),
+    TAG_NOT_FOUND(404, "Tag not found"),
+    ANSWER_NOT_FOUND(404, "Answer not found");
 
     @Getter
     private int status;
