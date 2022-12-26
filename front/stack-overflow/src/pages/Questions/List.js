a8a753f5f5861cde81994f9ed05d625eb1015f14
Atia <67799687+AtiaKR@users.noreply.github.com>
2022-12-27 08:09:03 +0900

    feat: Add spinner for loading screen

--- front/stack-overflow/src/pages/Questions/List.js
+++ front/stack-overflow/src/pages/Questions/List.js
@@ -1,9 +1,10 @@
-import Summary from "./Contents/Summary";
-import useStore from "./Store/SummaryStates";
-import TabButton from "./Buttons/TabButton";
-import { useEffect } from "react";
+import Summary from "../../components/Contents/Summary";
+import useStore from "../../store/SummaryStates";
+import TabButton from "../../components/Buttons/TabButton";
+import { useState, useEffect, CSSProperties } from "react";
 import styled from "styled-components";
-import Pagination from "./Contents/Pagination";
+import Pagination from "../../components/Contents/Pagination";
+import { PacmanLoader } from "react-spinners";
 
 const Container = styled.div`
   display: flex;
@@ -19,31 +20,46 @@ const QuestionNumber = styled.div`
   font-size: 17px;
 `;
 
+const SpinnerContainer = styled.div`
+  width: 100%;
+  height: 50vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const List = () => {
-  const { questions } = useStore();
+  const { questions, pageInfo } = useStore();
 
   return (
     <>
-      <Container>
-        <QuestionNumber>23,343,145 questions</QuestionNumber>
-        <TabButton />
-      </Container>
-      {questions
-        ? questions.map((el) => {
-            return (
-              <Summary
-                key={el.id}
-                title={el.title}
-                content={el.content}
-                tags={el.tags}
-                vote={el.votes}
-                answer={el.answerCount}
-                views={el.views}
-                bounty={50}
-              />
-            );
-          })
-        : null}
+      {pageInfo ? (
+        <Container>
+          <QuestionNumber>{pageInfo.totalElements} questions</QuestionNumber>
+          <TabButton />
+        </Container>
+      ) : null}
+      {questions ? (
+        questions.map((el, idx) => {
+          return (
+            <Summary
+              key={idx}
+              title={el.title}
+              content={el.content}
+              tags={el.tags}
+              vote={el.votes}
+              answer={el.answerCount}
+              views={el.views}
+              name={el.name}
+              userId={el.userId}
+            />
+          );
+        })
+      ) : (
+        <SpinnerContainer>
+          <PacmanLoader color="var(--black-200)" />
+        </SpinnerContainer>
+      )}
       <Pagination />
     </>
   );
