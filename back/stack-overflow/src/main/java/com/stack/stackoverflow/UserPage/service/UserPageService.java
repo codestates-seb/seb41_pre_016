9f8b3bd1c4df10c64de3862b4ee6b4c6cb2965c0
kung036 <sinariari3@gmail.com>
2022-12-26 16:48:17 +0900

    feat: UserPage
    Modify user-page API

--- back/stack-overflow/src/main/java/com/stack/stackoverflow/UserPage/service/UserPageService.java
+++ back/stack-overflow/src/main/java/com/stack/stackoverflow/UserPage/service/UserPageService.java
@@ -6,6 +6,8 @@ import com.stack.stackoverflow.user.entity.User;
 import com.stack.stackoverflow.user.service.UserService;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserPageService {
     private final UserPageRepository userPageRepository;
@@ -17,8 +19,30 @@ public class UserPageService {
         this.userService = userService;
     }
 
+    // 유효한 UserPage인지 검증
     public UserPage findUserPage(Long userId) {
         User user = userService.findUser(userId);
         return userPageRepository.findByUser(user);
+    }
+
+    // User 정보 업데이트
+    public UserPage updateUserPage(User user) {
+        // 유효한 UserPage인지 검증
+        UserPage userPage = findUserPage(user.getUserId());
+
+        // 유효한 User인지 검증
+        User findUser = userService.findVerifiedUser(user.getUserId());
+
+        // User 정보 업데이트
+        Optional.ofNullable(user.getName())
+                .ifPresent(name -> findUser.setName(name));
+        Optional.ofNullable(user.getEmail())
+                .ifPresent(email -> findUser.setEmail(email));
+        Optional.ofNullable(user.getPassword())
+                .ifPresent(password -> findUser.setPassword(password));
+
+        userPage.setUser(findUser);
+
+        return userPageRepository.save(userPage);
     }
 }
