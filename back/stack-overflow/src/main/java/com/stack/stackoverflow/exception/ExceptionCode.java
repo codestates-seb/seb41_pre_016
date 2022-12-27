2fd3cc8f918a9b18dcfa310e28f0049bd9bf867c
kung036 <sinariari3@gmail.com>
2022-12-27 22:41:58 +0900

    Fix: ExceptionCode
    add error code

--- back/stack-overflow/src/main/java/com/stack/stackoverflow/exception/ExceptionCode.java
+++ back/stack-overflow/src/main/java/com/stack/stackoverflow/exception/ExceptionCode.java
@@ -3,14 +3,14 @@ package com.stack.stackoverflow.exception;
 import lombok.Getter;
 
 public enum ExceptionCode {
-    MEMBER_NOT_FOUND(404, "Member not found"),
-    MEMBER_EXISTS(409, "Member exists"),
-    COFFEE_NOT_FOUND(404, "Coffee not found"),
-    COFFEE_CODE_EXISTS(409, "Coffee Code exists"),
-    ORDER_NOT_FOUND(404, "Order not found"),
-    CANNOT_CHANGE_ORDER(403, "Order can not change"),
-    NOT_IMPLEMENTATION(501, "Not Implementation"),
-    INVALID_MEMBER_STATUS(400, "Invalid member status"),
+//    MEMBER_NOT_FOUND(404, "Member not found"),
+//    MEMBER_EXISTS(409, "Member exists"),
+//    COFFEE_NOT_FOUND(404, "Coffee not found"),
+//    COFFEE_CODE_EXISTS(409, "Coffee Code exists"),
+//    ORDER_NOT_FOUND(404, "Order not found"),
+//    CANNOT_CHANGE_ORDER(403, "Order can not change"),
+//    NOT_IMPLEMENTATION(501, "Not Implementation"),
+//    INVALID_MEMBER_STATUS(400, "Invalid member status"),
     USER_NOT_FOUND(404, "User not found"),
     QUESTION_NOT_FOUND(404, "Question not found"),
     USER_EXISTS(409, "User exists"),
