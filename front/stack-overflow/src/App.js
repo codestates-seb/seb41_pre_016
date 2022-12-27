7381d73f615d6e05768ab4e936e1c6b396730877
LeeWang <99003659+Leewang31@users.noreply.github.com>
2022-12-27 09:46:23 +0900

    Revert "pages : Header, LeftSideBar, AskQuestion, Login, Signup  (#13)" (#16)
    This reverts commit 427a6661a1522df216de69713fd049b28df911a6.

--- front/stack-overflow/src/App.js
+++ front/stack-overflow/src/App.js
@@ -1,43 +1,31 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios';
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LeftSideBar from "./LeftSideBar/LeftSideBar";
-import Header from "./Header/Header";
-import styled from "styled-components";
-import HeaderDropDown from "./Header/HeaderDropDown";
-import Login from "./Login/Login";
-import MainPage from "./Page/MainPage";
-const RootDiv = styled.div`
-  z-index: -100;
-  display: flex;
-  padding-top: 50px;
-`;
-const MainDiv = styled.div`
-  max-width: 1264px;
-  width: 100%;
-  background: none;
-  display: flex;
-  justify-content: space-between;
-  margin: 0 auto;
-  position: relative;
-  flex: 1 0 auto;
-  text-align: left;
-`;
 const App = () => {
-  return (
-    <BrowserRouter>
-      <RootDiv>
-        <Header />
-      </RootDiv>
-      <MainDiv>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </MainDiv>
-    </BrowserRouter>
-  );
+  const URL = '/test';
+
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+
+      try {
+        const response = await axios.get(URL);
+        console.log(response);
+        setData(response);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  return <div>Hello</div>;
 };
 
 export default App;
