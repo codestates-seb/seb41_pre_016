622d60910011d3b463da8c9c56aa01dc78d5f58d
bimilless <bimilless@gmail.com>
2022-12-28 16:38:33 +0900

    feat: 초기 Controller, entity delete

--- back/stack-overflow/src/main/java/com/stack/stackoverflow/comment/controller/CommentController.java
+++ back/stack-overflow/src/main/java/com/stack/stackoverflow/comment/controller/CommentController.java
@@ -1,40 +1,40 @@
-package com.stack.stackoverflow.comment.controller;
-
-import com.stack.stackoverflow.comment.dto.CommentDto;
-import com.stack.stackoverflow.comment.entity.Comment;
-import com.stack.stackoverflow.comment.mapper.CommentMapper;
-import com.stack.stackoverflow.comment.service.CommentService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-
-@CrossOrigin(origins = "*", allowedHeaders = "*")
-@RestController
-@RequestMapping("/answer/{answer-id}/comment")
-@Validated
-@Slf4j
-public class CommentController {
-
-    private final CommentService commentService;
-    private final CommentMapper mapper;
-
-    public CommentController(CommentService commentService, CommentMapper mapper) {
-
-        this.commentService = commentService;
-        this.mapper = mapper;
-    }
-
-    @PostMapping
-    public ResponseEntity postComment(@Valid @RequestBody CommentDto.Post requestBody) {
-        Comment comment =
-                commentService.createComment(mapper.commentPostDtoToComment(requestBody));
-
-        return new ResponseEntity<>(
-                (mapper.commentToCommentResponseDto(comment)),
-                HttpStatus.CREATED);
-    }
-}
+//package com.stack.stackoverflow.comment.controller;
+//
+//import com.stack.stackoverflow.comment.dto.CommentDto;
+//import com.stack.stackoverflow.comment.entity.Comment;
+//import com.stack.stackoverflow.comment.mapper.CommentMapper;
+//import com.stack.stackoverflow.comment.service.CommentService;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.validation.annotation.Validated;
+//import org.springframework.web.bind.annotation.*;
+//
+//import javax.validation.Valid;
+//
+//@CrossOrigin(origins = "*", allowedHeaders = "*")
+//@RestController
+//@RequestMapping("/answer/{answer-id}/comment")
+//@Validated
+//@Slf4j
+//public class CommentController {
+//
+//    private final CommentService commentService;
+//    private final CommentMapper mapper;
+//
+//    public CommentController(CommentService commentService, CommentMapper mapper) {
+//
+//        this.commentService = commentService;
+//        this.mapper = mapper;
+//    }
+//
+//    @PostMapping
+//    public ResponseEntity postComment(@Valid @RequestBody CommentDto.Post requestBody) {
+//        Comment comment =
+//                commentService.createComment(mapper.commentPostDtoToComment(requestBody));
+//
+//        return new ResponseEntity<>(
+//                (mapper.commentToCommentResponseDto(comment)),
+//                HttpStatus.CREATED);
+//    }
+//}
