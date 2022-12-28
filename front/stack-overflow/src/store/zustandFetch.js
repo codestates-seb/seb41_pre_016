89697e9256ad2ba87931cfe169304d935db1b4f4
LeeWang <99003659+Leewang31@users.noreply.github.com>
2022-12-28 15:20:09 +0900

    refactor: Refactor fetch way

--- front/stack-overflow/src/store/zustandFetch.js
+++ front/stack-overflow/src/store/zustandFetch.js
@@ -2,9 +2,18 @@ import create from "zustand";
 import axios from "axios";
 
 export const fetchStore = create((set) => ({
-  item: {},
+  data: null,
+  isLoading: false,
+  error: null,
   fetch: async (url) => {
+    set({ isLoading: true });
+    try{
     const response = await axios.get(url);
-    set({ item: await response });
+    set({ data: await response });
+    }catch (e){
+      console.log(e);
+      set({ error: e.message });
+    }
+    set({ isLoading: false });
   },
 }));
