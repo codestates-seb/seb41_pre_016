ac14485f5190b5e9cb4e6071b6071215f357a5b0
LeeWang <99003659+Leewang31@users.noreply.github.com>
2022-12-28 09:16:05 +0900

    Revert "fix : ignore (#17)" (#18)
    This reverts commit 8deff38b7bc774d5d459312cea9b46bf8040590b.

--- front/stack-overflow/src/App.js
+++ front/stack-overflow/src/App.js
@@ -1,33 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
-import TagsPage from './pages/TagsPage/index';
-import Users from './pages/Users/index';
 
 const App = () => {
-  // const URL = process.env.REACT_APP_API_URL;
+  const URL = '/test';
 
-  // const [data, setData] = useState();
-  // const [loading, setLoading] = useState(false);
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     setLoading(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
 
-  //     try {
-  //       const response = await axios.get(URL);
-  //       console.log(response);
-  //       setData(response);
-  //     } catch (e) {
-  //       console.log(e);
-  //     }
-  //     setLoading(false);
-  //   };
+      try {
+        const response = await axios.get(URL);
+        console.log(response);
+        setData(response);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
 
-  //   fetchData();
-  // }, []);
+    fetchData();
+  }, []);
 
-  return <Users />;
+  return <div>Hello</div>;
 };
 
 export default App;
