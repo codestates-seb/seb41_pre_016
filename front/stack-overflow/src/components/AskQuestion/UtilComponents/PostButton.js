0c7610dfc2cd51445d36b96062738b77f97ce6f7
LeeWang <99003659+Leewang31@users.noreply.github.com>
2022-12-30 14:06:06 +0900

    feat: Change how user write (textarea->toast ui)

--- front/stack-overflow/src/components/AskQuestion/UtilComponents/PostButton.js
+++ front/stack-overflow/src/components/AskQuestion/UtilComponents/PostButton.js
@@ -1,15 +1,22 @@
 import { QuestionStore } from "../../../store/zustandQuestion";
+import {useCookies} from "react-cookie";
 
-const PostButton = () => {
-  const { title, content, tags, postQuestion } = QuestionStore();
+const PostButton = ({Editor,editorRef}) => {
+  const [cookies, setCookie, removeCookie] = useCookies(['access_jwt']);
+  const { title, tags, postQuestion } = QuestionStore();
   const postButton = () => {
+    const content = editorRef.current?.getInstance().getHTML();
     const questionObj = {
       title,
       content,
       tags,
     };
-    postQuestion("/question", 8, questionObj);
+    const cookieObj={
+      Authorization:cookies.access_jwt.Authorization,
+      Refresh:cookies.access_jwt.Refresh,
+    }
+    postQuestion("/question", 6, questionObj,cookieObj);
   };
-  return <button onClick={postButton}>Post your question</button>;
+  return <button onClick={postButton} className='blueButton'>Post your question</button>;
 };
 export default PostButton;
