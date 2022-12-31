792d6d5df84de9ec8e2a5d81574ebbb36a888d88
LeeWang <99003659+Leewang31@users.noreply.github.com>
2022-12-31 19:51:30 +0900

    merge fix

--- front/stack-overflow/src/pages/MainPage.js
+++ front/stack-overflow/src/pages/MainPage.js
@@ -1,20 +1,17 @@
 import LeftSideBar from "../components/LeftSideBar/LeftSideBar";
 import useFetch from "../store/useFetch";
-import Questions from "./Questions/Questions";
-
-//import for testing
-import Article from "./Article/Article";
 
 const MainPage = () => {
   const [data, isPending, error] = useFetch("/user/1");
   return (
     <>
       <LeftSideBar />
-      <Questions>
-        {error && <div>error</div>}
-        {isPending && <div>Loading</div>}
-        {data && console.log("HI")}
-      </Questions>
+      {error && <div>error</div>}
+      {isPending && <div>Loading</div>}
+      {data && console.log("HI")}
+      <a href="/questions/ask">
+        <button>Ask question</button>
+      </a>
     </>
   );
 };
