958f1f5964f467b73dfaa8c789526db030226fc0
kung036 <sinariari3@gmail.com>
2023-01-02 23:37:24 +0900

    chore: update build(.jar)

--- back/stack-overflow/src/main/java/com/stack/stackoverflow/user/controller/UserController.java
+++ back/stack-overflow/src/main/java/com/stack/stackoverflow/user/controller/UserController.java
@@ -25,7 +25,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-// @CrossOrigin(origins = "", allowedHeaders = "*")
+@CrossOrigin(origins = "", allowedHeaders = "*")
 @RestController
 @RequestMapping("/user")
 @Validated
