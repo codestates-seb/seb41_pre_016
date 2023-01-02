aa8482e30723000553a777a11e9406f0f7af2b54
kung036 <sinariari3@gmail.com>
2023-01-02 23:21:49 +0900

    chore: update build(.jar)

--- back/stack-overflow/src/main/java/com/stack/stackoverflow/config/WebConfig.java
+++ back/stack-overflow/src/main/java/com/stack/stackoverflow/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.stack.stackoverflow.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,7 +18,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:8080",
                         "http://pre-project-fontend.s3-website.ap-northeast-2.amazonaws.com/",
                         "http://ec2-13-209-69-139.ap-northeast-2.compute.amazonaws.com:8080/") // 허용할 출처
-                .allowedMethods("GET", "POST", "PATCH", "DELETE","OPTIONS") // 허용할 HTTP Method
+                .allowedMethods( HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.PATCH.name()) // 허용할 HTTP Method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000) // 원하는 시간만큼 pre-flight request를 캐싱
                 .exposedHeaders("authorization");
